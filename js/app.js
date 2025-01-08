9efdc27e263e83c5d2d942adfdc45cd361846bae
gabrielcampari <camparicorreia@gmail.com>
2025-01-08 11:02:20 -0300

    feat/confirm devolution game button

--- js/app.js
+++ js/app.js
@@ -1,15 +1,32 @@
+let hireGames = 0;
+
+function showHireGames() {
+  console.log(`O número de jogos alugados atualmente são de ${hireGames}`);
+}
+
 function changeStatus(id) {
   let chosenGame = document.getElementById(`game-${id}`);
   let button = chosenGame.querySelector(".dashboard__item__button");
   let image = chosenGame.querySelector(".dashboard__item__img");
 
   if (image.classList.contains("dashboard__item__img--rented")) {
-    image.classList.remove("dashboard__item__img--rented");
-    button.classList.remove("dashboard__item__button--return");
-    button.textContent = "Alugar";
+    if (confirm("Tem certeza que deseja devolver este jogo?")) {
+      image.classList.remove("dashboard__item__img--rented");
+      button.classList.remove("dashboard__item__button--return");
+      button.textContent = "Alugar";
+      hireGames--;
+    }
   } else {
     image.classList.add("dashboard__item__img--rented");
     button.classList.add("dashboard__item__button--return");
     button.textContent = "Devolver";
+    hireGames++;
   }
+
+  showHireGames();
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  hireGames = document.querySelectorAll(".dashboard__item__img--rented").length;
+  showHireGames();
+});
